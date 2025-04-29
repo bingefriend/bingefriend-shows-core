30c9124f8432f469bf98af6d21d0873350b08d5d
Tom Boone <tbc@tomboone.com>
2025-04-28 22:47:57 -0400

    add webchannel to __init__

--- src/bingefriend/shows/core/models/__init__.py
+++ src/bingefriend/shows/core/models/__init__.py
@@ -1,11 +1,12 @@
 """SQLAlchemy models for the BingeFriend application."""
 
-from .base import Base
-from .show import Show
-from .season import Season
-from .episode import Episode
-from .network import Network
-from .genre import Genre
-from .show_genre import ShowGenre
+from bingefriend.shows.core.models.base import Base
+from bingefriend.shows.core.models.show import Show
+from bingefriend.shows.core.models.season import Season
+from bingefriend.shows.core.models.episode import Episode
+from bingefriend.shows.core.models.network import Network
+from bingefriend.shows.core.models.web_channel import WebChannel
+from bingefriend.shows.core.models.genre import Genre
+from bingefriend.shows.core.models.show_genre import ShowGenre
 
-__all__ = ["Base", "Show", "Season", "Episode", "Network", "Genre", "ShowGenre"]
+__all__ = ["Base", "Show", "Season", "Episode", "Network", "WebChannel", "Genre", "ShowGenre"]
